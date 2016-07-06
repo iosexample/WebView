4dbc87742134210e0c1b2cccc0678890e0f63609
Don <donly@magicalboy.com>
2016-07-06 11:03:19 +0800

    update test target

--- WebViewTests/WebViewTests.m
+++ WebViewTests/WebViewTests.m
@@ -27,9 +27,10 @@
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
 }
 
-- (void)testPerformanceExample {
+- (void)DISABLED_testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
